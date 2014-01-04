fa4d0f99f56bcaea776ec260fa8803a6e1d5e90a
Daniel Reiter <semibmx@gmail.com>
2014-01-04 15:42:00 -0600

    testing trinary

--- misc/TrinaryNode.java
+++ misc/TrinaryNode.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList; // for printing out the tree
+import java.util.*; // for printing out the tree
 
 class TrinaryNode {
 	int data;
@@ -39,8 +39,8 @@ class TrinaryNode {
 	/**
 	* Deletes all occurences of val from the tree.
 	* 
-	* Removes a node by changing the parent's child reference
-	* of this node to point to the first node's right child,
+	* Removes a node by changing this parent's child reference
+	* of this node to point to this node's right child,
 	* or then left child if no right child, or then null if neither.
 	*/
 	public void remove(int val) {
@@ -53,18 +53,38 @@ class TrinaryNode {
 				curNode.leftChild = this.leftChild;
 				this.leftChild.parent = curNode;
 
-				if (this.data < this.parent.data)
-					this.parent.leftChild = this.rightChild;
-				else
-					this.parent.rightChild = this.rightChild;
-				this.rightChild.parent = this.parent;
+				if (this.parent == null) { // head
+					this.data = this.rightChild.data;
+					this.leftChild = this.rightChild.leftChild;
+					this.leftChild.parent = this;
+					this.middleChild = this.rightChild.middleChild;
+					this.middleChild.parent = this;
+					this.rightChild = this.rightChild.rightChild;
+					this.rightChild.parent = this;
+				} else {
+					if (this.data < this.parent.data)
+						this.parent.leftChild = this.rightChild;
+					else
+						this.parent.rightChild = this.rightChild;
+					this.rightChild.parent = this.parent;
+				}
 			}
 			else if (this.leftChild != null) {
-				if (this.data < this.parent.data)
-					this.parent.leftChild = this.leftChild;
-				else
-					this.parent.rightChild = this.leftChild;
-				this.leftChild.parent = this.parent;
+				if (this.parent == null) { // head
+					this.data = this.leftChild.data;
+					this.rightChild = this.leftChild.rightChild;
+					this.rightChild.parent = this;
+					this.middleChild = this.leftChild.middleChild;
+					this.middleChild.parent = this;
+					this.leftChild = this.leftChild.leftChild;
+					this.leftChild.parent = this;
+				} else {
+					if (this.data < this.parent.data)
+						this.parent.leftChild = this.leftChild;
+					else
+						this.parent.rightChild = this.leftChild;
+					this.leftChild.parent = this.parent;
+				}
 			}
 			else {
 				if (this.data < this.parent.data) {
@@ -79,12 +99,13 @@ class TrinaryNode {
 		}
 	}
 
-	public void printTreeBreadth() {
+	public String printTreeBreadth() {
+		StringBuilder res = new StringBuilder();
 		LinkedList <TrinaryNode> q = new LinkedList <TrinaryNode> ();
 		q.add(this);
 		do {
 			TrinaryNode curNode = q.poll();
-			System.out.println (curNode.data);
+			res.append(curNode.data + " ");
 			if (curNode.leftChild != null)
 				q.add(curNode.leftChild);
 			if (curNode.middleChild != null)
@@ -92,44 +113,25 @@ class TrinaryNode {
 			if (curNode.rightChild != null)
 				q.add(curNode.rightChild);
 		} while (!q.isEmpty());
+
+		res.deleteCharAt(res.length()-1); // remove trailing space
+		
+		return res.toString();
 	}
 
-	public void printTreePreorder() {
-		System.out.println(this.data);
+	public String printTreePreorder() {
+		StringBuilder res = new StringBuilder();
+		res.append(this.data + " ");
 		if (this.leftChild != null)
-			this.leftChild.printTreePreorder();
+			res.append(this.leftChild.printTreePreorder() + " ");
 		if (this.middleChild != null)
-			this.middleChild.printTreePreorder();
+			res.append(this.middleChild.printTreePreorder() + " ");
 		if (this.rightChild != null)
-			this.rightChild.printTreePreorder();
-	}
-
-
-	public static void main(String[] args) {
-		TrinaryNode head = new TrinaryNode(1);
-		head.insert(4);
-		head.insert(2);
-		head.insert(3);
-		head.insert(2);
-		head.insert(4);
-		head.insert(1);
-		head.insert(6);
-		head.insert(5);
-		head.insert(6);
-		head.insert(7);
-		head.insert(5);
-		System.out.println("Breadth-first traversal");
-		head.printTreeBreadth();
-		System.out.println("\nPreorder traversal");
-		head.printTreePreorder();
-
-		head.remove(4);
-		System.out.println("\n\nRemoved 4");
-		System.out.println("Breadth-first traversal");
-		head.printTreeBreadth();
-		System.out.println("\nPreorder traversal");
-		head.printTreePreorder();
-		System.out.println("\nRan");
-	}
+			res.append(this.rightChild.printTreePreorder() + " ");
 	
+		String result = res.toString().replaceAll("\\s+", " "); // remove extra spaces
+
+		return result.substring(0, result.length()-1);
+	}
+
 }
